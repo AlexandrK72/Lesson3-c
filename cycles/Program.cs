ad0738cb26c36f05dcc57907f4d025e28cca07a9
Alexandr <chika431sis@yandex.ru>
2023-10-02 23:36:23 +0500

    работа с циклами

---
+++ cycles/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// цикл for
+
+
+string Method4(int count, string text)
+{
+
+    string result = String.Empty; // result - изначально будет пустой строкой
+    for (int i = 0; i < count; i++)
+
+        {
+            result = result + text;
+
+        }
+    return result; // указывается та переменная, которую мы хотим получить из метода.
+}
+
+string res = Method4(5, "текст 4 метода, ");
+Console.WriteLine(res);
+
+// цикл в цикле (таблица умножния)
+
+
+for (int i = 2; i <= 10; i++)
+{
+   for (int j = 2; j <= 10; j++)
+   {
+    Console.WriteLine($"{i} x {j} = {i*j}");   //интерполяция строк {}
+   }
+    Console.WriteLine(); // сделано для разрыва строк (пустая строка)
+}
---
+++ task cicle/Program.cs
@@ -0,0 +1,48 @@
+﻿//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача? ( что значит дан текст? Что значит черточками? Какой алфавит?)
+
+
+
+
+
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = “qwerty”
+// 012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}"; // если текст совпал с текстом оторое хотим заменить, то вкладываем новое значение
+        else result = result + $"{text[i]}"; // иначе совпадения нет, то в reault  добавляем тот же символ
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(text, 'к', 'К');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'С', 'с');
+
+Console.WriteLine(newText);
+
+
+
